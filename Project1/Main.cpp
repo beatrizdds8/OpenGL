8968c9f062f0b9911d43215c9659543788e9916b
Beatriz Duarte <beatrizduarteds8@gmail.com>
2024-08-27 22:23:18 -0300

    Add shaderClass

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
-#include<stdio.h>
 
 // Copy&Paste shader scene
 // Vertex Shader source code
@@ -31,15 +30,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Make a array of data type gl float (normal is allowed but can differ in size from the floats that openGL uses, so is safer)
-	// Every 3 gl floats will represent one coordinate, in this case the coordinates were choose to create a equilateral triangle, we can choose whatever
-	// as long it is between -1 and 1
-	GLfloat vertices[] = {
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
-	};
-
+	
 
 	/*criar um objeto janela passando os parâmetros de largura, altura, nome, se é fullscreen ou não, e um quinto parâmetro irrelevante 
 	para o projeto no momento por isso NULL*/
@@ -89,14 +80,34 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	// Make a array of data type gl float (normal is allowed but can differ in size from the floats that openGL uses, so is safer)
+	// Every 3 gl floats will represent one coordinate, in this case the coordinates were choose to create a equilateral triangle, we can choose whatever
+	// as long it is between -1 and 1
+	GLfloat vertices[] = {
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		- 0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	};
+
+	GLuint indices[] = {
+		0,3,5, // Lower left triangle
+		3,2,4, // Lower right triangle
+		5,4,1 // Upper triangle
+	};
+
+
 	// Tell opengl how interpret our vertices
 	// creating a vertex buffer object where we’ll store our vertex data, VBO is a array of vertices, but here we just need one
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	glGenVertexArrays(1, &VAO); // has to be generate always before the VBO!!
 
 	//create the buffer object, 1 as the first object because we only have 1 3d object
 	glGenBuffers(1,&VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
@@ -109,6 +120,9 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	// Tell opengl where to find our packed object
 	// This stores pointers to one or more VBOs, and tells opengl how to interpret them.
 	// VAOs exist in order to quickly be able to switch between different VBOs.
@@ -127,7 +141,8 @@ int main() {
 	// *Optional: to be 100% sure you won't accidentally change VBO or VBA with a function
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	/**/
 	// front buffer = where information is being read to display in the screen
 	// back buffer = where information is being writen
 
@@ -137,12 +152,11 @@ int main() {
 
 	// Executar o comando que dizemos para preparar, nesse caso especificamos o comando do buffer de cor (existem tipos de buffer)
 	glClear(GL_COLOR_BUFFER_BIT);
-
+	
 	// A cor esta no buffer que não esta sendo mostrado na tela, entao temos que trocar os buffers
 	glfwSwapBuffers(window);
 
-
-
+	
 	//criar um loop para a janela fechar somente ao clicar no botão de fechar(quando uma função especificamente dizer pra fechar), senão ela aparecerá e sumirá rapidamente
 	while (!glfwWindowShouldClose(window)) {
 		
@@ -153,7 +167,7 @@ int main() {
 		glBindVertexArray(VAO);
 		// For the drawing function itself, specifying the type of primitive(triangles), starting index of the vertices(0) and the amount 
 		// of vertices we want to draw(3)
-		glDrawArrays(GL_TRIANGLES,0,3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		// Make sure we swap the buffers so that the image gets updated each frame.
 		glfwSwapBuffers(window);
 
@@ -165,6 +179,7 @@ int main() {
 	// Delete objects that we created
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 
